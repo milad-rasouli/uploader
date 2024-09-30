aa45c55e0facedc2598fbb94b23f1331d5ba86fe
mahdi <mahdimehrabi17@gmail.com>
2024-09-30 13:23:28 +0330

    feat: no need for GeneratePublicURL

--- file.go
+++ file.go
@@ -7,6 +7,5 @@ import (
 
 type FileRepository interface {
 	UploadPublicFile(ctx context.Context, bucketName, objectName, contentType string, file io.Reader) error
-	GeneratePublicURL(bucketName string, objectName string) string
 	DeleteFile(ctx context.Context, bucketName string, objectName string) error
 }
