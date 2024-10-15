552cb129a6906f42a67ffdc24db35d104917e48b
mahdi <mahdimehrabi17@gmail.com>
2024-10-15 12:59:19 +0330

    feat: read csv file

---
+++ csv.go
@@ -0,0 +1,22 @@
+package uploader
+
+import (
+	"encoding/csv"
+	"os"
+)
+
+func ReadCSVFile(filePath string) ([][]string, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return records, nil
+}
